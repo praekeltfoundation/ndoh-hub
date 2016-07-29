7f87b386ffd5b7a00198a301b74e8285f26527d6
gsvr <george@westerncapelabs.com>
2016-07-29 12:47:01 +0200

    remove commented out code

--- changes/views.py
+++ changes/views.py
@@ -14,11 +14,3 @@ class ChangePost(mixins.CreateModelMixin, generics.GenericAPIView):
         source = Source.objects.get(user=self.request.user)
         request.data["source"] = source.id
         return self.create(request, *args, **kwargs)
-
-    # TODO make this work in test harness, works in production
-    # def perform_create(self, serializer):
-    #     serializer.save(created_by=self.request.user,
-    #                     updated_by=self.request.user)
-
-    # def perform_update(self, serializer):
-    #     serializer.save(updated_by=self.request.user)
