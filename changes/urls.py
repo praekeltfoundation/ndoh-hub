6b0df22de7c28f3a3a07420007669d43cbabb3f4
gsvr <george@westerncapelabs.com>
2016-07-29 12:46:08 +0200

    remove url duplication

--- changes/urls.py
+++ changes/urls.py
@@ -1,12 +1,9 @@
-from django.conf.urls import url, include
-from rest_framework import routers
+from django.conf.urls import url
 from . import views
 
-router = routers.DefaultRouter()
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browseable API.
 urlpatterns = [
-    url(r'^api/v1/', include(router.urls)),
     url(r'^api/v1/change/', views.ChangePost.as_view()),
 ]
