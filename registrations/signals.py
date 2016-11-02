b17df72a6b816239cb0f43a0c7b16d547a218ce4
Simon de Haan <simon@praekelt.org>
2016-11-02 12:53:12 +0100

    remove unused code

--- registrations/signals.py
+++ registrations/signals.py
@@ -8,35 +8,3 @@ def psh_validate_subscribe(sender, instance, created, **kwargs):
         from .tasks import validate_subscribe
         validate_subscribe.apply_async(
             kwargs={"registration_id": str(instance.id)})
-
-
-# def psh_push_registration_to_jembi(sender, instance, raw, **kwargs):
-#     """
-#     Pre-save hook to push registrations to Jembi, only schedule the push
-#     if the validated state flips from False to True.
-#
-#     NOTE:   It is important that this is a pre-save hook because we want to
-#             track changes between a previous record version (non-existent
-#             or in the db) and what we're saving now to be able to track
-#             a `validated` state going from `False` to `True`
-#     """
-#     # raw is when data is loaded with existing data like fixtures
-#     if raw:
-#         return
-#
-#     registrations = Registration.objects.all()
-#     if registrations.filter(pk=instance.pk).exists():
-#         # Fetch the registration as it is in the database so we can
-#         # compare the values we're about to save to the database
-#         db_registration = Registration.objects.get(pk=instance.pk)
-#         # If it wasn't validated before but is now then
-#         # we need to notify Jembi
-#         if not db_registration.validated and instance.validated:
-#             from .tasks import push_registration_to_jembi
-#             push_registration_to_jembi.apply_async(
-#                 kwargs={"registration_id": str(instance.id)})
-#     elif instance.validated:
-#         # If it's new but validated then we also need to notify Jembi
-#         from .tasks import push_registration_to_jembi
-#         push_registration_to_jembi.apply_async(
-#             kwargs={"registration_id": str(instance.id)})
