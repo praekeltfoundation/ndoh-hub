8b356c21275982a4278ec2e2ead313bdbf16dc46
Jason Paidoussi <jason@paidoussi.net>
2017-01-03 11:25:00 +0200

    Added registrations created metric

--- registrations/signals.py
+++ registrations/signals.py
@@ -8,3 +8,14 @@ def psh_validate_subscribe(sender, instance, created, **kwargs):
         from .tasks import validate_subscribe
         validate_subscribe.apply_async(
             kwargs={"registration_id": str(instance.id)})
+
+
+def psh_fire_created_metric(sender, instance, created, **kwargs):
+    """ Post save hook to fire Registration created metric
+    """
+    if created:
+        from .tasks import fire_metric
+        fire_metric.apply_async(kwargs={
+            "metric_name": 'registrations.created.sum',
+            "metric_value": 1.0
+        })
