60147361cfc2b998fb527c982883b90e0454c525
Kaitlyn Crawford <kaitlyn@praekelt.com>
2017-10-30 14:00:00 +0200

    Remove json print statement since it just encodes the content

--- registrations/views.py
+++ registrations/views.py
@@ -238,7 +238,6 @@ class JembiHelpdeskOutgoingView(APIView):
             result.raise_for_status()
         except (requests.exceptions.HTTPError,) as e:
             print e.response.text
-            print e.response.json()
             print e.response.status_code
             return Response(
                 'Error when posting to Jembi. Body: %s Payload: %r' % (
