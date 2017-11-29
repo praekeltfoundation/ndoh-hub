98933b5a2086f1e88a3a09bf501f99a7afec4803
Rudi Giesler <rudi@praekelt.org>
2017-11-29 09:50:42 +0200

    Use full version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ndoh-hub",
-    version="0.1",
+    version="0.1.0",
     url='http://github.com/praekeltfoundation/ndoh-hub',
     license='BSD',
     author='Praekelt Foundation',
