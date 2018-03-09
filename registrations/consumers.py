e51c37766c03d020330cf80cc377bea6671cef32
Rudi Giesler <rudi@praekelt.org>
2018-03-09 14:45:56 +0200

    Add registration action

--- registrations/consumers.py
+++ registrations/consumers.py
@@ -1,4 +1,7 @@
 from channels.generic.websocket import JsonWebsocketConsumer
+from rest_framework.exceptions import ValidationError
+
+from registrations.views import JembiAppRegistration
 
 
 class JembiAppRegistrationConsumer(JsonWebsocketConsumer):
@@ -9,13 +12,28 @@ class JembiAppRegistrationConsumer(JsonWebsocketConsumer):
         else:
             self.close()
 
+    def action_registration(self, data: dict) -> None:
+        """
+        Attempts to create a new registration, and returns the result of the
+        new registration
+        """
+        try:
+            JembiAppRegistration.create_registration(
+                self.user, data)
+        except ValidationError as e:
+            self.send_json({
+                'registration_id': data.get('external_id'),
+                'registration_data': data,
+                'status': 'validation_failed',
+                'error': e.detail,
+            })
+
     def receive_json(self, content: dict) -> None:
         action = content.get('action', None)
         data = content.get('data', {})
 
         if action == 'registration':
-            # TODO: Add registration logic
-            pass
+            self.action_registration(data)
         elif action == 'status':
             # TODO: Add status logic
             pass
--- registrations/test_consumers.py
+++ registrations/test_consumers.py
@@ -1,18 +1,26 @@
+import datetime
 import pytest
 from channels.db import database_sync_to_async
 from channels.testing import WebsocketCommunicator
 from django.contrib.auth.models import User
+from unittest import mock
 
+from registrations import views
 from registrations.consumers import JembiAppRegistrationConsumer
+from registrations.models import Source
+from ndoh_hub import utils
 
 
 @database_sync_to_async
 def create_user() -> User:
-    return User.objects.create_user('test')
+    user = User.objects.create_user('test')
+    Source.objects.create(user=user)
+    return user
 
 
 @database_sync_to_async
 def cleanup_user(user: User) -> None:
+    user.sources.all().delete()
     user.delete()
 
 
@@ -76,3 +84,74 @@ async def test_unrecognised_action() -> None:
 
     await communicator.disconnect()
     await cleanup_user(user)
+
+
+@pytest.mark.asyncio
+@pytest.mark.django_db
+async def test_registration_validation_error() -> None:
+    """
+    For the registration action, if there is a validation error, it should be
+    returned.
+    """
+    user = await create_user()
+    communicator = await create_communicator(user)
+
+    await communicator.send_json_to({
+        'action': 'registration',
+        'data': {
+            'external_id': 'test-external',
+        },
+    })
+    response = await communicator.receive_json_from()
+    assert response['registration_id'] == 'test-external'
+    assert response['registration_data'] == {
+        'external_id': 'test-external',
+    }
+    assert response['status'] == 'validation_failed'
+    assert response['error']['mom_edd'] == ['This field is required.']
+
+    await communicator.disconnect()
+    await cleanup_user(user)
+
+
+@pytest.mark.asyncio
+@pytest.mark.django_db
+async def test_registration_valid() -> None:
+    """
+    For the registration action, if it is valid, then it should be sent to the
+    celery task.
+    """
+    old_today = utils.get_today
+    today = utils.get_today = mock.MagicMock()
+    today.return_value = datetime.datetime(2016, 1, 1).date()
+
+    old_task = views.validate_subscribe_jembi_app_registration
+    task = views.validate_subscribe_jembi_app_registration = mock.MagicMock()
+
+    user = await create_user()
+    communicator = await create_communicator(user)
+
+    await communicator.send_json_to({
+        'action': 'registration',
+        'data': {
+            'external_id': 'test-external-id',
+            'mom_edd': '2016-06-06',
+            'mom_msisdn': '+27820000000',
+            'mom_consent': True,
+            'created': '2016-01-01 00:00:00',
+            'hcw_msisdn': '+27821111111',
+            'clinic_code': '123456',
+            'mom_lang': 'eng_ZA',
+            'mha': 1,
+            'mom_dob': '1988-01-01',
+            'mom_id_type': 'none',
+        },
+    })
+
+    await communicator.disconnect()
+
+    task.delay.assert_called_once()
+
+    await cleanup_user(user)
+    utils.get_today = old_today
+    views.validate_subscribe_jembi_app_registration = old_task
