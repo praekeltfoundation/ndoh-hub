c3f967358e7ef69bf3626ad8b4d82fd1e7234840
Rudi Giesler <rudi@praekelt.org>
2018-04-16 12:55:38 +0200

    Release 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ndoh-hub",
-    version="0.2.0",
+    version="0.2.1",
     url='http://github.com/praekeltfoundation/ndoh-hub',
     license='BSD',
     author='Praekelt Foundation',
