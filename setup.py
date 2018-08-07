88ae1d4ed470bb7375e8bb1fc478f9786a3fb70f
Rudi Giesler <rudi@praekelt.org>
2018-08-07 15:09:13 +0200

    Update package classifier to actual python version

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
         'Framework :: Django',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
