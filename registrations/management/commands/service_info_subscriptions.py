b339b8c6abd288e5cd9c71115cf20ebb73e51573
Rudi Giesler <rudi@praekelt.org>
2018-08-23 15:57:33 +0200

    Add basic logic for service info subs management command

---
+++ registrations/management/commands/service_info_subscriptions.py
@@ -0,0 +1,115 @@
+from os import environ
+import logging
+
+from django.core.management.base import BaseCommand, CommandError
+from registrations.models import SubscriptionRequest
+
+from ndoh_hub.utils import sbm_client
+
+
+class Command(BaseCommand):
+    log = logging.getLogger("registrations")
+    help = ('Subscribes all current WhatsApp subscription users to the '
+            'correct place in the service info messageset')
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--sbm-url', dest='sbm_url',
+            default=environ.get('STAGE_BASED_MESSAGING_URL'),
+            help=('The Stage Based Messaging Service for '
+                  'service info subscriptions'))
+        parser.add_argument(
+            '--sbm-token', dest='sbm_token',
+            default=environ.get('STAGE_BASED_MESSAGING_TOKEN'),
+            help=('The Authorization token for the SBM Service')
+        )
+
+    def handle(self, *args, **kwargs):
+        sbm_url = kwargs['sbm_url']
+        sbm_token = kwargs['sbm_token']
+
+        if not sbm_url:
+            raise CommandError(
+                'Please make sure either the STAGE_BASED_MESSAGING_URL '
+                'environment variable or --sbm-url is set.')
+
+        if not sbm_token:
+            raise CommandError(
+                'Please make sure either the STAGE_BASED_MESSAGING_TOKEN '
+                'environment variable or --sbm-token is set.')
+
+        messageset_mapping = {
+            ms['id']: ms['short_name']
+            for ms in sbm_client.get_messagesets()["results"]
+        }
+
+        service_info_messageset = next(sbm_client.get_messagesets(
+            {"short_name": "whatsapp_service_info.hw_full.1"})["results"])
+        print(service_info_messageset)
+
+        subscriptions = sbm_client.get_subscriptions({
+            "messageset_contains": "whatsapp_momconnect_",
+            "active": True,
+        })["results"]
+
+        for subscription in subscriptions:
+            if self.identity_has_service_info_subscription(
+                    messageset_mapping, subscription["identity"]):
+                # They already have an active service info subscription, skip
+                self.log.debug(
+                    "%s already has a service info subscription, skipping",
+                    subscription["identity"])
+                continue
+
+            sequence = self.service_info_sequence(
+                messageset_mapping[subscription["messageset"]],
+                subscription["next_sequence_number"]
+            )
+            self.log.debug(
+                "Creating service info subscription request for %s",
+                subscription["identity"])
+            SubscriptionRequest.objects.create(
+                identity=subscription["identity"],
+                messageset=service_info_messageset["id"],
+                next_sequence_number=sequence,
+                lang=subscription["lang"],
+                schedule=service_info_messageset["default_schedule"],
+            )
+
+    def service_info_sequence(self, short_name: str, sequence: int) -> int:
+        """
+        Calculates the sequence number that the service info messageset should
+        be on, given the existing subscription that a user has
+        """
+        # TODO: work out the actual logic here
+        # whatsapp_momconnect_prebirth.hw_partial.1
+        # whatsapp_momconnect_postbirth.hw_full.1
+        # whatsapp_momconnect_postbirth.hw_full.2
+        # whatsapp_momconnect_prebirth.hw_full.1
+        # whatsapp_momconnect_prebirth.hw_full.2
+        # whatsapp_momconnect_prebirth.hw_full.3
+        # whatsapp_momconnect_prebirth.hw_full.4
+        # whatsapp_momconnect_prebirth.hw_full.5
+        # whatsapp_momconnect_prebirth.hw_full.6
+        # whatsapp_momconnect_prebirth.patient.1
+        return 1
+
+    def identity_has_service_info_subscription(
+            self, messageset_mapping: dict, identity: str) -> bool:
+        """
+        Whether or not the given identity has an active subscription to the
+        service info messageset.
+
+        Args:
+            messageset_mapping (dict):
+                Mapping between messageset keys and names
+            identity (str): The UUID of the identity
+        """
+        subscriptions = [
+            messageset_mapping[sub["messageset"]] for sub in
+            sbm_client.get_subscriptions({
+                "active": True,
+                "identity": identity,
+            })["results"]
+        ]
+        return "whatsapp_service_info.hw_full.1" in subscriptions
---
+++ registrations/management/commands/tests/test_service_info_subscriptions.py
@@ -0,0 +1,95 @@
+from django.core.management import call_command
+from django.test import TestCase
+import responses
+
+from ndoh_hub import utils_tests
+from registrations.management.commands.service_info_subscriptions import (
+    Command)
+from registrations.models import SubscriptionRequest
+
+
+class ServiceInfoSubscriptionsTests(TestCase):
+    @responses.activate
+    def test_identity_has_service_info_subscription_true(self):
+        """
+        If the identity has an active service info subscription, should return
+        True
+        """
+        utils_tests.mock_get_subscriptions(
+            "?active=True&identity=identity-uuid", [
+                {"messageset": 1}, {"messageset": 2}])
+        cmd = Command()
+        res = cmd.identity_has_service_info_subscription({
+            1: "whatsapp_momconnect_prebirth.hw_full.1",
+            2: "whatsapp_service_info.hw_full.1",
+            3: "whatsapp_popi.hw_full.1",
+        }, "identity-uuid")
+        self.assertTrue(res)
+
+    @responses.activate
+    def test_identity_has_service_info_subscription_false(self):
+        """
+        If the identity does not have an active service info subscription,
+        should return False
+        """
+        utils_tests.mock_get_subscriptions(
+            "?active=True&identity=identity-uuid", [
+                {"messageset": 1}, {"messageset": 3}])
+        cmd = Command()
+        res = cmd.identity_has_service_info_subscription({
+            1: "whatsapp_momconnect_prebirth.hw_full.1",
+            2: "whatsapp_service_info.hw_full.1",
+            3: "whatsapp_popi.hw_full.1",
+        }, "identity-uuid")
+        self.assertFalse(res)
+
+    @responses.activate
+    def test_service_info_subscriptions(self):
+        """
+        For each identity that has an active subscription, should create a
+        service info subscription request if they don't already have one.
+        """
+        utils_tests.mock_get_messagesets([
+            {"id": 1, "short_name": "whatsapp_momconnect_prebirth.hw_full.1"},
+            {"id": 2, "short_name": "whatsapp_service_info.hw_full.1"},
+            {"id": 3, "short_name": "whatsapp_popi.hw_full.1"},
+        ])
+        utils_tests.mock_get_messageset_by_shortname(
+            "whatsapp_service_info.hw_full.1")
+        utils_tests.mock_get_subscriptions(
+            "?active=True&messageset_contains=whatsapp_momconnect_", [
+                {
+                    "identity": "identity-uuid1",
+                    "lang": "zul_ZA",
+                    "messageset": 1,
+                    "sequence_number": 5,
+                    "next_sequence_number": 3,
+                },
+                {
+                    "identity": "identity-uuid2",
+                    "lang": "sso_ZA",
+                    "messageset": 1,
+                    "sequence_number": 7,
+                    "next_sequence_number": 5,
+                },
+            ]
+        )
+        utils_tests.mock_get_subscriptions(
+            "?active=True&identity=identity-uuid1", [
+                {"messageset": 1}, {"messageset": 2}])
+        utils_tests.mock_get_subscriptions(
+            "?active=True&identity=identity-uuid2", [
+                {"messageset": 1}, {"messageset": 3}])
+
+        call_command(
+            "service_info_subscriptions",
+            "--sbm-url", "http://sbm/",
+            "--sbm-token", "sbmtoken"
+        )
+
+        [subreq] = SubscriptionRequest.objects.all()
+        self.assertEqual(subreq.messageset, 95)
+        self.assertEqual(subreq.identity, "identity-uuid2")
+        self.assertEqual(subreq.next_sequence_number, 1)
+        self.assertEqual(subreq.lang, "sso_ZA")
+        self.assertEqual(subreq.schedule, 123)
