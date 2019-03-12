c142be948509c59bd719d51c5b4b1ad3149f7ea8
Rudi Giesler <rudi@praekelt.org>
2019-03-12 14:05:40 +0200

    Add script for submitting whatsapp template CSV to API

---
+++ scripts/migrate_to_whatsapp_templates/template_submission.py
@@ -0,0 +1,80 @@
+"""
+This script takes in a CSV on stdin with the fields "name", "language", "content",
+which reflects the name of the template, the language that the template is for, as well
+as the content of that template.
+
+It then outputs each template in the format expected by the WhatsApp API. If
+--execute is set, it submits the template to the WhatsApp API using the authorization
+token specified with --token.
+"""
+import argparse
+import csv
+import json
+import sys
+import urllib.parse
+
+import requests
+
+from ndoh_hub.constants import WHATSAPP_LANGUAGE_MAP
+
+
+WHATSAPP_URL = "https://whatsapp.praekelt.org"
+WHATSAPP_TEMPLATE_URL = urllib.parse.urljoin(WHATSAPP_URL, "/v1/message_templates")
+
+session = requests.Session()
+
+
+def whatsapp_api_format(template):
+    """
+    Takes a dictionary with name, language, and content keys, and outputs a dictionary
+    of the template in the format expected by the whatsapp API.
+    """
+    return {
+        "category": "ALERT_UPDATE",
+        "content": template["content"],
+        "name": template["name"],
+        "language": WHATSAPP_LANGUAGE_MAP[template["language"]],
+    }
+
+
+def submit_to_whatsapp(token, template):
+    """
+    Submits the template to the WhatsApp API
+    """
+    r = session.post(
+        url=WHATSAPP_TEMPLATE_URL,
+        json=template,
+        headers={
+            "Authorization": "Bearer {}".format(token),
+            "Accept": "application/vnd.v1+json",
+        },
+    )
+    r.raise_for_status()
+
+
+def parse_arguments(args):
+    parser = argparse.ArgumentParser(
+        description="Submits templates to WhatsApp. Takes a CSV on stdin"
+    )
+    parser.add_argument(
+        "--execute",
+        action="store_true",
+        help="Whether to actually submit template to the WhatsApp API",
+    )
+    parser.add_argument("--token", help="The token to use for the WhatsApp API")
+    return parser.parse_args(args)
+
+
+def run(input, output, args=None):
+    arguments = parse_arguments(args)
+    reader = csv.DictReader(input)
+    for r in reader:
+        template = whatsapp_api_format(r)
+        output.write(json.dumps(template))
+        output.write("\n")
+        if arguments.execute:
+            submit_to_whatsapp(arguments.token, template)
+
+
+if __name__ == "__main__":
+    run(sys.stdin, sys.stdout)
---
+++ scripts/migrate_to_whatsapp_templates/tests/test_template_submission.py
@@ -0,0 +1,157 @@
+import csv
+import json
+from io import StringIO
+import unittest
+from unittest.mock import patch
+
+from requests.exceptions import HTTPError
+import responses
+
+from scripts.migrate_to_whatsapp_templates import template_submission
+
+
+class TemplateSubmissionTests(unittest.TestCase):
+    def test_parse_arguments_default(self):
+        """ Defaults to execute being false """
+        arguments = template_submission.parse_arguments([])
+        self.assertEqual(arguments.execute, False)
+        self.assertEqual(arguments.token, None)
+
+    def test_parse_arguments_non_default(self):
+        """ Specifying execute makes it true, value of token is stored """
+        arguments = template_submission.parse_arguments(
+            ["--execute", "--token", "test-token"]
+        )
+        self.assertEqual(arguments.execute, True)
+        self.assertEqual(arguments.token, "test-token")
+
+    def test_whatsapp_api_format(self):
+        """ Returns dictionary in correct format for WhatsApp API """
+        result = template_submission.whatsapp_api_format(
+            {
+                "name": "test_name",
+                "language": "zul_ZA",
+                "content": "Test template {{1}}",
+            }
+        )
+        self.assertEqual(
+            result,
+            {
+                "category": "ALERT_UPDATE",
+                "content": "Test template {{1}}",
+                "name": "test_name",
+                "language": "uz",
+            },
+        )
+
+    @responses.activate
+    def test_submit_to_whatsapp(self):
+        """ Submits in the correct format with the correct headers """
+        responses.add(
+            responses.POST,
+            "https://whatsapp.praekelt.org/v1/message_templates",
+            json={},
+            status=200,
+        )
+        data = {
+            "category": "ALERT_UPDATE",
+            "content": "Test template {{1}}",
+            "name": "test_name",
+            "language": "uz",
+        }
+        template_submission.submit_to_whatsapp("test-token", data)
+
+        [call] = responses.calls
+        self.assertEqual(json.loads(call.request.body), data)
+        self.assertEqual(call.request.headers["Authorization"], "Bearer test-token")
+        self.assertEqual(call.request.headers["Accept"], "application/vnd.v1+json")
+
+    @responses.activate
+    def test_submit_to_whatsapp_failure(self):
+        """ Should raise an exception on invalid status code response """
+        responses.add(
+            responses.POST,
+            "https://whatsapp.praekelt.org/v1/message_templates",
+            json={},
+            status=500,
+        )
+        data = {
+            "category": "ALERT_UPDATE",
+            "content": "Test template {{1}}",
+            "name": "test_name",
+            "language": "uz",
+        }
+        with self.assertRaises(HTTPError):
+            template_submission.submit_to_whatsapp("test-token", data)
+
+    @patch(
+        "scripts.migrate_to_whatsapp_templates.template_submission.submit_to_whatsapp"
+    )
+    def test_run(self, submit_mock):
+        """ Submits to WhatsApp if execute is selected """
+        input = StringIO()
+
+        writer = csv.writer(input)
+        writer.writerow(["name", "language", "content"])
+        writer.writerow(["test1", "zul_ZA", "testcontent1"])
+        writer.writerow(["test2", "afr_ZA", "testcontent2"])
+
+        input = StringIO(input.getvalue())
+        output = StringIO()
+
+        template_submission.run(input, output, ["--execute", "--token", "test-token"])
+
+        submit_mock.assert_any_call(
+            "test-token",
+            {
+                "category": "ALERT_UPDATE",
+                "content": "testcontent1",
+                "name": "test1",
+                "language": "uz",
+            },
+        )
+
+        submit_mock.assert_any_call(
+            "test-token",
+            {
+                "category": "ALERT_UPDATE",
+                "content": "testcontent2",
+                "name": "test2",
+                "language": "af",
+            },
+        )
+
+    @responses.activate
+    def test_dry_run(self):
+        """ If --execute isn't supplied, no HTTP calls should be made """
+        input = StringIO()
+
+        writer = csv.writer(input)
+        writer.writerow(["name", "language", "content"])
+        writer.writerow(["test1", "zul_ZA", "testcontent1"])
+        writer.writerow(["test2", "afr_ZA", "testcontent2"])
+
+        input = StringIO(input.getvalue())
+        output = StringIO()
+
+        template_submission.run(input, output, ["--token", "test-token"])
+
+        self.assertEqual(len(responses.calls), 0)
+
+        self.assertEqual(
+            [json.loads(o) for o in output.getvalue().strip().split("\n")],
+            [
+                {
+                    "category": "ALERT_UPDATE",
+                    "content": "testcontent1",
+                    "name": "test1",
+                    "language": "uz",
+                },
+                {
+                    "category": "ALERT_UPDATE",
+                    "content": "testcontent2",
+                    "name": "test2",
+                    "language": "af",
+                },
+            ],
+        )
