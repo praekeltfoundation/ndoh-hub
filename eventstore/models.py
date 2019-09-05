119d0e4b8dab442a43f3326bb4509afcbc67b6ca
Rudi Giesler <rudi@praekelt.org>
2019-09-05 17:38:17 +0200

    Add optout model to event store

---
+++ eventstore/models.py
@@ -0,0 +1,48 @@
+import uuid
+
+from django.conf import settings
+from django.contrib.postgres.fields import JSONField
+from django.db import models
+from django.utils import timezone
+
+
+class OptOut(models.Model):
+    STOP_TYPE = "stop"
+    FORGET_TYPE = "forget"
+    LOSS_TYPE = "loss"
+    OPTOUT_TYPES = ((STOP_TYPE, "Stop"), (FORGET_TYPE, "Forget"), (LOSS_TYPE, "Loss"))
+
+    NOT_USEFUL_REASON = "not_useful"
+    OTHER_REASON = "other"
+    UNKNOWN_REASON = "unknown"
+    SMS_FAILURE_REASON = "sms_failure"
+    MISCARRIAGE_REASON = "miscarriage"
+    STILLBIRTH_REASON = "stillbirth"
+    BABYLOSS_REASON = "babyloss"
+    NOT_HIV_POSITIVE_REASON = "not_hiv_pos"
+    REASON_TYPES = (
+        (NOT_USEFUL_REASON, "Not useful"),
+        (OTHER_REASON, "Other"),
+        (UNKNOWN_REASON, "Unknown"),
+        (SMS_FAILURE_REASON, "SMS failure"),
+        (MISCARRIAGE_REASON, "Miscarriage"),
+        (STILLBIRTH_REASON, "Stillbirth"),
+        (BABYLOSS_REASON, "Lost baby"),
+        (NOT_HIV_POSITIVE_REASON, "Not HIV positive"),
+    )
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    contact_id = models.UUIDField()
+    optout_type = models.CharField(max_length=6, choices=OPTOUT_TYPES)
+    reason = models.CharField(max_length=11, choices=REASON_TYPES)
+    source = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(default=timezone.now)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True
+    )
+    data = JSONField(default=dict, blank=True, null=True)
+
+    def __str__(self):
+        return "{} opt out: {} <{}>".format(
+            self.get_optout_type_display(), self.get_reason_display(), self.contact_id
+        )
---
+++ eventstore/test_views.py
@@ -0,0 +1,59 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from eventstore.models import OptOut
+
+
+class OptOutViewSetTests(APITestCase):
+    def test_authentication_required(self):
+        """
+        There must be an authenticated user to make the request
+        """
+        response = self.client.post(reverse("optout-list"))
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_permission_required(self):
+        """
+        The authenticated user must have the correct permissions to make the request
+        """
+        user = get_user_model().objects.create_user("test")
+        self.client.force_authenticate(user)
+        response = self.client.post(reverse("optout-list"))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_data_validation(self):
+        """
+        The supplied data must be validated, and any errors returned
+        """
+        user = get_user_model().objects.create_user("test")
+        user.user_permissions.add(Permission.objects.get(codename="add_optout"))
+        self.client.force_authenticate(user)
+        response = self.client.post(reverse("optout-list"))
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_successful_request(self):
+        """
+        Should create a new OptOut object in the database
+        """
+        user = get_user_model().objects.create_user("test")
+        user.user_permissions.add(Permission.objects.get(codename="add_optout"))
+        self.client.force_authenticate(user)
+        response = self.client.post(
+            reverse("optout-list"),
+            {
+                "contact_id": "9e12d04c-af25-40b6-aa4f-57c72e8e3f91",
+                "optout_type": OptOut.STOP_TYPE,
+                "reason": OptOut.UNKNOWN_REASON,
+                "source": "SMS",
+            },
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        [optout] = OptOut.objects.all()
+        self.assertEqual(str(optout.contact_id), "9e12d04c-af25-40b6-aa4f-57c72e8e3f91")
+        self.assertEqual(optout.optout_type, OptOut.STOP_TYPE)
+        self.assertEqual(optout.reason, OptOut.UNKNOWN_REASON)
+        self.assertEqual(optout.source, "SMS")
+        self.assertEqual(optout.created_by, user)
