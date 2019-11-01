5f1804be6625ad209144ced1e794024f6478e059
Rudi Giesler <rudi@praekelt.org>
2019-11-01 16:29:54 +0200

    Change jembi submission command to use JembiSubmission

---
+++ registrations/management/commands/jembi_submit_events.py
@@ -0,0 +1,63 @@
+from uuid import UUID
+
+from celery import chain
+from django.core.management import BaseCommand, CommandError
+from django.utils import timezone
+from django.utils.dateparse import parse_datetime
+
+from registrations.tasks import push_to_jembi_api
+
+
+class Command(BaseCommand):
+
+    help = (
+        "Submit events to Jembi. This command is useful if for some reason events "
+        "weren't submitted or failed to submit, or if the submission was switched off "
+        "due to issues or outages downstream."
+    )
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--since",
+            type=parse_datetime,
+            default=None,
+            help="Filter for timestamp since (YYYY-MM-DD HH:MM:SS)",
+        )
+        parser.add_argument(
+            "--until",
+            type=parse_datetime,
+            default=None,
+            help="Filter for timestamp until (YYYY-MM-DD HH:MM:SS)",
+        )
+        parser.add_argument(
+            "--all",
+            action="store_true",
+            help="Submit all events, not only ones that haven't been submitted",
+        )
+        parser.add_argument(
+            "--submit",
+            action="store_true",
+            help="Actually submits the events to Jembi, instead of doing a dry run",
+        )
+
+    def handle(self, *args, **options):
+        from registrations.models import JembiSubmission
+
+        if options["all"]:
+            events = JembiSubmission.objects.all()
+        else:
+            events = JembiSubmission.objects.filter(submitted=False)
+
+        if options["since"]:
+            events = events.filter(timestamp__gte=timezone.make_aware(options["since"]))
+        if options["until"]:
+            events = events.filter(timestamp__lte=timezone.make_aware(options["until"]))
+
+        self.stdout.write(f"Submitting {events.count()} events.")
+
+        if not options["submit"]:
+            self.stdout.write("WARNING: --submit not specified, not submitting events")
+            return
+
+        for event in events.values_list("pk", "path", "request_data").iterator():
+            push_to_jembi_api.delay(event)
---
+++ registrations/management/commands/tests/test_jembi_submit_events.py
@@ -0,0 +1,63 @@
+from unittest.mock import patch
+from datetime import datetime
+import pytz
+
+from django.core.management import call_command
+from django.test import TestCase
+
+from registrations.models import JembiSubmission
+
+
+class JembiSubmitEventsTests(TestCase):
+    @patch("registrations.management.commands.jembi_submit_events.push_to_jembi_api")
+    def test_since_filter(self, task):
+        """
+        Should not submit any events before the "since" date
+        """
+        s = JembiSubmission.objects.create(request_data={})
+        s.timestamp = datetime(2016, 1, 1, tzinfo=pytz.UTC)
+        s.save()
+        call_command(
+            "jembi_submit_events", since=datetime(2016, 1, 2), submit=True, all=True
+        )
+        task.delay.assert_not_called()
+
+    @patch("registrations.management.commands.jembi_submit_events.push_to_jembi_api")
+    def test_until_filter(self, task):
+        """
+        Should not submit any events after the "until" date
+        """
+        s = JembiSubmission.objects.create(request_data={})
+        s.timestamp = datetime(2016, 1, 2, tzinfo=pytz.UTC)
+        s.save()
+        call_command(
+            "jembi_submit_events", until=datetime(2016, 1, 1), submit=True, all=True
+        )
+        task.delay.assert_not_called()
+
+    @patch("registrations.management.commands.jembi_submit_events.push_to_jembi_api")
+    def test_all_filter(self, task):
+        """
+        Should not submit already submitted events if "--all" is not specified
+        """
+        JembiSubmission.objects.create(request_data={}, submitted=True)
+        call_command("jembi_submit_events", submit=True, all=False)
+        task.delay.assert_not_called()
+
+    @patch("registrations.management.commands.jembi_submit_events.push_to_jembi_api")
+    def test_submit_filter(self, task):
+        """
+        If submit isn't specified, then we should not submit the events
+        """
+        JembiSubmission.objects.create(request_data={})
+        call_command("jembi_submit_events")
+        task.delay.assert_not_called()
+
+    @patch("registrations.management.commands.jembi_submit_events.push_to_jembi_api")
+    def test_submit_event(self, task):
+        """
+        If the event should be submitted, then we should submit it
+        """
+        s = JembiSubmission.objects.create(path="test", request_data={"test": "data"})
+        call_command("jembi_submit_events", submit=True)
+        task.delay.assert_called_once_with((s.id, "test", {"test": "data"}))
