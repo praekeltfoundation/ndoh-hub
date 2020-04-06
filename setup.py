3f055ee59ebc8ad2fed8541b02959ba6518ee774
Erik Harding <erik@praekelt.org>
2020-04-06 15:54:39 +0200

    Release  0.8.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ndoh-hub",
-    version="0.8.7",
+    version="0.8.9",
     url="http://github.com/praekeltfoundation/ndoh-hub",
     license="BSD",
     author="Praekelt Foundation",
