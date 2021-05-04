42386b98bf4f9118f5d837d3d941012f1e57c0fc
devchima <developer.chima@gmail.com>
2021-05-04 09:11:15 -0400

    Changes and url index

--- ada/models.py
+++ ada/models.py
@@ -1,28 +1,41 @@
 from django.db import models
 
-# Create your models here.
-
 
 class RedirectUrl(models.Model):
 
-    urls = models.CharField(max_length=255, blank=True, null=True)
-    content = models.TextField(default='This url has no copy')
+    url = models.URLField(
+        max_length=255,
+        blank=True,
+        null=True,
+        default="https://hub.momconnect.za/confirmredirect",
+    )
+    content = models.TextField(default="This entry has no copy")
+    refresh_url = models.CharField(
+        max_length=200, null=False, blank=False, default="http://127.0.0.1:8000"
+    )
+    symptom_check_url = models.CharField(
+        max_length=200, null=False, blank=False, default="http://symptomcheck.co.za"
+    )
+    parameter = models.IntegerField(null=True, blank=True)
     time_stamp = models.DateTimeField(auto_now=True)
 
     def my_counter(self):
-        total_number = RedirectUrlsEntry.objects.filter(urls=self.id).count()
+        total_number = RedirectUrlsEntry.objects.filter(url=self.id).count()
         return total_number
 
     def __str__(self):
-        if self.urls:
-            return f'ID - {self.id} for {self.urls}/{self.id} | Clicked {self.my_counter()} times | Content: {self.content}'
+        if self.url:
+            return (f"{self.parameter}: {self.url}/{self.id} \n"
+                    f"| Clicked {self.my_counter()} times | Content: {self.content}")
 
 
 class RedirectUrlsEntry(models.Model):
-    urls = models.ForeignKey(RedirectUrl,
-                             on_delete=models.CASCADE, blank=True, null=True)
+    url = models.ForeignKey(
+        RedirectUrl, on_delete=models.CASCADE, blank=True, null=True
+    )
     time_stamp = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        if self.urls:
-            return f'{self.urls.urls} with ID {self.urls.id}  was visited at {self.time_stamp}'
+        if self.url:
+            return (f"{self.url.url} with ID {self.url.id} \n"
+                    f"was visited at {self.time_stamp}")
--- ada/views.py
+++ ada/views.py
@@ -1,24 +1,27 @@
-from django.shortcuts import render, redirect, HttpResponse, HttpResponseRedirect
-from .models import *
-# Create your views here.
+from django.shortcuts import render, HttpResponseRedirect
+from .models import RedirectUrl, RedirectUrlsEntry
 
 
 def clickActivity(request, pk):
 
     try:
         redirect_url = RedirectUrl.objects.get(id=pk)
-
-        store_url_entry = RedirectUrlsEntry(urls=redirect_url)
+        store_url_entry = RedirectUrlsEntry(url=redirect_url)
         store_url_entry.save()
-        my_link = f'http://symptomcheck.co.za/?whatsappid={request.GET.get("whatsappid")}'
-        return HttpResponseRedirect(my_link)
+        destination_url = (
+            f'{redirect_url.symptom_check_url}'
+            f'/?whatsappid={request.GET.get("whatsappid")}'
+                )
+        return HttpResponseRedirect(destination_url)
     except Exception as e:
-        return render(request, 'index.html', {'error': e})
+        return render(request, "index.html", {"error": e})
 
-def default_page(request,pk):
+
+def default_page(request, pk):
     try:
-        redirect_url = RedirectUrl.objects.get(id=pk)
         whatsappid = request.GET.get("whatsappid")
-        return render(request, 'meta_refresh.html', {'pk':pk,'whatsappid':whatsappid})
+        return render(
+            request, "meta_refresh.html", {"pk": pk, "whatsappid": whatsappid}
+        )
     except Exception as e:
-        return render(request, 'index.html', {'error': e})
+        return render(request, "index.html", {"error": e})
