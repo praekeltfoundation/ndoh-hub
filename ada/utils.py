c22ad36bfc022da43751baa79bc7365d1dacc983
devchima <developer.chima@gmail.com>
2021-05-24 06:07:48 -0400

    Tests | settings

--- ada/utils.py
+++ ada/utils.py
@@ -4,5 +4,5 @@ from django.conf import settings
 from temba_client.v2 import TembaClient
 
 rapidpro = None
-if settings.ADA_SYMPTOMCHECK_V2:
+if settings.EXTERNAL_REGISTRATIONS_V2:
     rapidpro = TembaClient(settings.RAPIDPRO_URL, settings.RAPIDPRO_TOKEN)
