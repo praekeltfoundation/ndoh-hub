4bffed3339834a579fe016cb6571b6174dfc544a
devchima <developer.chima@gmail.com>
2021-06-24 07:29:50 -0400

    case changes and build fix

--- ada/views.py
+++ ada/views.py
@@ -1,6 +1,6 @@
 from urllib.parse import urlencode
-from django.conf import settings
 
+from django.conf import settings
 from django.http import Http404, HttpRequest, HttpResponse
 from django.shortcuts import HttpResponseRedirect, render
 from rest_framework import generics, permissions, status
@@ -48,9 +48,9 @@ def clickActivity(request: HttpRequest, pk: int, whatsappid: str) -> HttpRespons
     else:
         store_url_entry = RedirectUrlsEntry(symptom_check_url=redirect_url)
         store_url_entry.save()
-        customizationId = settings.ADA_CUSTOMIZATION_ID
+        customization_id = settings.ADA_CUSTOMIZATION_ID
         url = f"{redirect_url.symptom_check_url}"
-        qs = urlencode({"whatsappid": whatsappid, "customizationId": customizationId})
+        qs = urlencode({"whatsappid": whatsappid, "customizationId": customization_id})
         destination_url = f"{url}?{qs}"
         return HttpResponseRedirect(destination_url)
 
