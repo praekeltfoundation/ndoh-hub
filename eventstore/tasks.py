15e2e55e0c231b75552f83eb8f1af9bb4ee96fb4
Erik Harding <erik@praekelt.org>
2021-11-03 09:49:12 +0200

    remove healthcheck complete task

--- eventstore/tasks.py
+++ eventstore/tasks.py
@@ -220,30 +220,6 @@ forget_contact = (
     soft_time_limit=10,
     time_limit=15,
 )
-def mark_turn_contact_healthcheck_complete(msisdn):
-    if settings.HC_TURN_URL is None or settings.HC_TURN_TOKEN is None:
-        return
-    contact_id = msisdn.lstrip("+")
-    url = urljoin(settings.HC_TURN_URL, f"v1/contacts/{contact_id}/profile")
-    response = requests.patch(
-        url,
-        json={"healthcheck_completed": True},
-        headers={
-            "Authorization": f"Bearer {settings.HC_TURN_TOKEN}",
-            "Accept": "application/vnd.v1+json",
-        },
-    )
-    response.raise_for_status()
-
-
-@app.task(
-    autoretry_for=(RequestException, SoftTimeLimitExceeded),
-    retry_backoff=True,
-    max_retries=15,
-    acks_late=True,
-    soft_time_limit=10,
-    time_limit=15,
-)
 def archive_turn_conversation(urn, message_id, reason):
     headers = {
         "Authorization": "Bearer {}".format(settings.TURN_TOKEN),
--- eventstore/test_tasks.py
+++ eventstore/test_tasks.py
@@ -15,32 +15,6 @@ def override_get_today():
     return datetime.datetime.strptime("20200117", "%Y%m%d").date()
 
 
-class MarkTurnContactHealthCheckCompleteTests(TestCase):
-    @override_settings(HC_TURN_URL=None, HC_TURN_TOKEN=None)
-    @responses.activate
-    def test_skips_when_no_settings(self):
-        """
-        Should not send anything if the settings aren't set
-        """
-        tasks.mark_turn_contact_healthcheck_complete("+27820001001")
-        self.assertEqual(len(responses.calls), 0)
-
-    @override_settings(HC_TURN_URL="https://turn", HC_TURN_TOKEN="token")
-    @responses.activate
-    def test_send_successful(self):
-        """
-        Should send with correct request data
-        """
-        responses.add(
-            responses.PATCH, "https://turn/v1/contacts/27820001001/profile", json={}
-        )
-        tasks.mark_turn_contact_healthcheck_complete("+27820001001")
-        [call] = responses.calls
-        self.assertEqual(json.loads(call.request.body), {"healthcheck_completed": True})
-        self.assertEqual(call.request.headers["Authorization"], "Bearer token")
-        self.assertEqual(call.request.headers["Accept"], "application/vnd.v1+json")
-
-
 class HandleOldWaitingForHelpdeskContactsTests(TestCase):
     def setUp(self):
         tasks.get_today = override_get_today
