dae96f697123701a22239ed2d5be636f24241b5b
Buhle <buhle@praekelt.org>
2021-12-22 16:35:05 +0200

    removed print statements

--- eventstore/random_contacts.py
+++ eventstore/random_contacts.py
@@ -12,15 +12,14 @@ from ndoh_hub.utils import rapidpro, get_today
 
 if settings.EXTERNAL_REGISTRATIONS_V2:
     if settings.TURN_URL and settings.TURN_TOKEN:
-        turn_api = settings.TURN_URL + '/v1/contacts/{}/messages'
+        turn_api = settings.TURN_URL + "/v1/contacts/{}/messages"
         turn_header = {
             "Authorization": "Bearer {}".format(settings.TURN_TOKEN),
-            "Accept": "application/vnd.v1+json"
+            "Accept": "application/vnd.v1+json",
         }
 
     if settings.RAPIDPRO_URL and settings.RAPIDPRO_TOKEN:
-        # url = urljoin(settings.RAPIDPRO_URL, '/api/v2/contacts.json')
-        rapidpro_url = urljoin(settings.RAPIDPRO_URL, '/contact/read/{}/')
+        rapidpro_url = urljoin(settings.RAPIDPRO_URL, "/contact/read/{}/")
 
 
 @app.task(
@@ -37,13 +36,17 @@ def get_contacts():
     after = get_today() - datetime.timedelta(days=6)
     contact_details = []
     try:
-        for contact_batch in rapidpro.get_contacts(before=before, after=after).iterfetches(retry_on_rate_exceed=True):
+        for contact_batch in rapidpro.get_contacts(
+            before=before, after=after
+        ).iterfetches(retry_on_rate_exceed=True):
             for contact in contact_batch:
                 contact_uuid = contact.uuid
                 contact_urns = contact.urns
 
                 if contact_uuid and contact_urns:
-                    whatsapp_number = [i.split(':')[1] for i in contact_urns if 'whatsapp' in i]
+                    whatsapp_number = [
+                        i.split(":")[1] for i in contact_urns if "whatsapp" in i
+                    ]
 
                     if whatsapp_number:
                         contact = whatsapp_number[0]
@@ -51,18 +54,19 @@ def get_contacts():
 
                         if profile_link:
                             rapidpro_link = rapidpro_url.format(contact_uuid)
-                            links = {'Rapid Pro Link: ': rapidpro_link, "Turn Link": profile_link}
+                            links = {
+                                "Rapid Pro Link: ": rapidpro_link,
+                                "Turn Link": profile_link,
+                            }
 
                             contact_details.append(links)
 
         if contact_details:
-            sent = True
-            # sent = send_slack_message(contact_details)
-            return {'success': sent, 'results': contact_details}
-        return {'success': False, 'results': contact_details}
+            sent = send_slack_message(contact_details)
+            return {"success": sent, "results": contact_details}
+        return {"success": False, "results": contact_details}
     except Exception as e:
-        print(e)
-        return {'success': False, 'results': contact_details}
+        return {"success": False, "results": contact_details}
 
 
 def get_turn_profile_link(contact):
@@ -76,28 +80,28 @@ def get_turn_profile_link(contact):
 
             if profile:
                 # Get turn message link
-                turn_link = profile.json().get('chat').get('permalink')
+                turn_link = profile.json().get("chat").get("permalink")
         except Exception as e:
             return turn_link
     return turn_link
 
 
 def send_slack_message(contact_details):
-    print('Contacts: ****', settings.SLACK_URL, settings.SLACK_TOKEN)
     # Send message to slack
     try:
-        response = requests.post(urljoin(settings.SLACK_URL, '/api/chat.postMessage'),
-                      {
-                        'token': settings.SLACK_TOKEN,
-                        'channel': 'test-mon',
-                        'text': contact_details
-                      }).json()
+        response = requests.post(
+            urljoin(settings.SLACK_URL, "/api/chat.postMessage"),
+            {
+                "token": settings.SLACK_TOKEN,
+                "channel": "test-mon",
+                "text": contact_details,
+            },
+        ).json()
         if response:
-            if response['ok']:
+            if response["ok"]:
                 return True
             return False
     except Exception as e:
-        print('Error: ', e)
         return False
 
 
--- eventstore/test_random_contacts.py
+++ eventstore/test_random_contacts.py
@@ -1,12 +1,10 @@
-from unittest import mock
 from django.test import TestCase, override_settings
 from temba_client.v2 import TembaClient
-# from random_contacts import get_contacts
-import random_contacts
+
+from eventstore import random_contacts
 import responses
 
 
-@override_settings(TURN_URL="https://turn", TURN_TOKEN="turn_token")
 class HandleRapidProContactsTests(TestCase):
     def setUp(self):
         random_contacts.rapidpro = TembaClient("textit.in", "test-token")
@@ -20,20 +18,23 @@ class HandleRapidProContactsTests(TestCase):
     def test_get_contacts(self):
         responses.add(
             responses.GET,
-            'https://rapidpro/api/v2/contacts.json',
-            json=[{'uuid': '148947f5-a3b6-4b6b-9e9b-25058b1b7800',
-                  'urns': ['whatsapp:277124567']
-                   },
-                  {
-                    'uuid': '128947f5-a3b6-4b3b-9e9b-25058b1b7801',
-                    'urns': ['whatsapp:27720001010', 'tel:0102584697']
-                }]
+            "https://rapidpro/api/v2/contacts.json",
+            json=[
+                {
+                    "uuid": "148947f5-a3b6-4b6b-9e9b-25058b1b7800",
+                    "urns": ["whatsapp:277124567"],
+                },
+                {
+                    "uuid": "128947f5-a3b6-4b3b-9e9b-25058b1b7801",
+                    "urns": ["whatsapp:27720001010", "tel:0102584697"],
+                },
+            ],
         )
-        
+
         response = random_contacts.get_contacts()
 
         self.assertEqual(type(response), dict)
-        self.assertEqual(len(response.get('results')), 2)
+        self.assertEqual(len(response.get("results")), 2)
 
 
 class HandleTurnProfileTests(TestCase):
@@ -41,34 +42,51 @@ class HandleTurnProfileTests(TestCase):
         random_contacts.rapidpro = TembaClient("textit.in", "test-token")
 
     @responses.activate
-    # @override_settings(TURN_URL="https://turn", TURN_TOKEN="token")
+    @override_settings(TURN_URL="http://turn", TURN_TOKEN="token")
     def test_get_turn_profile_link(self):
         responses.add(
             responses.GET,
-            'http://turn/v1/contacts/27781234567/messages',
-            json={'chat': {'assigned_to': None, 'owner': '+27836378500',
-                           'permalink': 'https://app.turn.io/c/68cc14b3-6a4e-4962-82ed-c572c6836fdd',
-                           'state': 'OPEN', 'state_reason': 'Re-opened by inbound message.',
-                           'unread_count': 0, 'uuid': '68cc14b3-6a4e-4962-82ed-c572c6836fdd'}},
+            "http://turn/v1/contacts/27781234567/messages",
+            json={
+                "chat": {
+                    "assigned_to": None,
+                    "owner": "+27836378500",
+                    "permalink": "https://app.turn.io/c/68cc14b3-6a4e-4962-82ed-c572c6836fdd",
+                    "state": "OPEN",
+                    "state_reason": "Re-opened by inbound message.",
+                    "unread_count": 0,
+                    "uuid": "68cc14b3-6a4e-4962-82ed-c572c6836fdd",
+                }
+            },
             status=200,
         )
-        response = random_contacts.get_turn_profile_link('27781234567')
-        print(str(response))
+        response = random_contacts.get_turn_profile_link("27781234567")
+
         self.assertEqual(type(response), str)
-        self.assertEqual(str(response), 'https://app.turn.io/c/68cc14b3-6a4e-4962-82ed-c572c6836fdd')
-        self.assertNotEqual(type(response), 'https://app.turn.io/c/68cc14b3-6a4e-4962-82ed-c572c6836fdz')
+        self.assertEqual(
+            str(response), "https://app.turn.io/c/68cc14b3-6a4e-4962-82ed-c572c6836fdd"
+        )
+        self.assertNotEqual(
+            type(response), "https://app.turn.io/c/68cc14b3-6a4e-4962-82ed-c572c6836fdz"
+        )
 
     @responses.activate
     def test_get_turn_profile_link_none_contact(self):
-        # Test if sent none contact to return no value
         contact = None
         responses.add(
             responses.GET,
-            'http://turn/v1/contacts//messages',
-            json={'chat': {'assigned_to': None, 'owner': '+27836378500',
-                           'permalink': 'https://app.turn.io/c/68cc14b3-6a4e-4962-82ed-c572c6836fdc',
-                           'state': 'OPEN', 'state_reason': 'Re-opened by inbound message.',
-                           'unread_count': 0, 'uuid': '68cc14b3-6a4e-4962-82ed-c572c6836fdc'}},
+            "http://turn/v1/contacts//messages",
+            json={
+                "chat": {
+                    "assigned_to": None,
+                    "owner": "+27836378500",
+                    "permalink": "https://app.turn.io/c/68cc14b3-6a4e-4962-82ed-c572c6836fdc",
+                    "state": "OPEN",
+                    "state_reason": "Re-opened by inbound message.",
+                    "unread_count": 0,
+                    "uuid": "68cc14b3-6a4e-4962-82ed-c572c6836fdc",
+                }
+            },
             status=200,
         )
         response = random_contacts.get_turn_profile_link(contact)
@@ -78,31 +96,36 @@ class HandleTurnProfileTests(TestCase):
 
 class HandleSendSlackMessage(TestCase):
     def setUp(self):
-        self.contact_details = [{'Rapid_Pro_Link: ':
-                            'http://rapidpro.qa.momconnect.co.za/contact/read/d62aabca-7b6c-4d32-a017-1a30a8a27663/',
-                            'Turn_Profile_Link': 'http://app.turn.io/c/68cc14b3-6a4e-4962-82ed-c572c6836fdc'},
-                           {'Rapid_Pro_Link: ': 'http://rapidpro.qa.momconnect.co.za/contact/read/b18cb592-5ee9-4e77-\
-                           a21c-f155df2a405d/',
-                            'Turn_Profile_Link': 'http://app.turn.io/c/0e10681a-13a2-4284-b961-d099a34bf74d'},
-                           {'Rapid_Pro_Link: ': 'http://rapidpro.qa.momconnect.co.za/contact/read/388412d0-22d1-469b-\
-                           b0ab-230c55170903/',
-                            'Turn_Profile_Link': 'http://app.turn.io/c/cda524fb-80b9-40c0-a3e2-a452b725b697'}]
+        self.contact_details = [
+            {
+                "Rapid_Pro_Link: ": "http://rapidpro.qa.momconnect.co.za/contact/read/d62aabca-7b6c-4d32-a017-1a30a8a27663/",
+                "Turn_Profile_Link": "http://app.turn.io/c/68cc14b3-6a4e-4962-82ed-c572c6836fdc",
+            },
+            {
+                "Rapid_Pro_Link: ": "http://rapidpro.qa.momconnect.co.za/contact/read/b18cb592-5ee9-4e77-\
+                           a21c-f155df2a405d/",
+                "Turn_Profile_Link": "http://app.turn.io/c/0e10681a-13a2-4284-b961-d099a34bf74d",
+            },
+            {
+                "Rapid_Pro_Link: ": "http://rapidpro.qa.momconnect.co.za/contact/read/388412d0-22d1-469b-\
+                           b0ab-230c55170903/",
+                "Turn_Profile_Link": "http://app.turn.io/c/cda524fb-80b9-40c0-a3e2-a452b725b697",
+            },
+        ]
 
     @responses.activate
     @override_settings(SLACK_URL="http://slack.com", SLACK_TOKEN="slack_token")
     def test_send_slack_message(self):
         responses.add(
             responses.POST,
-            'http://slack.com/api/chat.postMessage',
+            "http://slack.com/api/chat.postMessage",
             json={
-                'token': 'slack_token',
-                'channel': 'test-mon',
-                'text': self.contact_details
-            }
+                "token": "slack_token",
+                "channel": "test-mon",
+                "text": self.contact_details,
+            },
         )
 
         response = random_contacts.send_slack_message(self.contact_details)
 
         self.assertEqual(response, True)
-
-
