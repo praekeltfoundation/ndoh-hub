0d96de0de3051eeb9d1cb851c37bbfd8254e21eb
Buhle <buhle@praekelt.org>
2021-12-24 07:38:59 +0200

    Review changes

--- eventstore/tasks.py
+++ eventstore/tasks.py
@@ -1,6 +1,6 @@
 import json
 import logging
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 from urllib.parse import urljoin
 
 import phonenumbers
@@ -8,9 +8,6 @@ import pytz
 import requests
 from celery.exceptions import SoftTimeLimitExceeded
 from django.conf import settings
-from requests.exceptions import RequestException
-from temba_client.exceptions import TembaHttpError
-
 from eventstore.models import (
     BabyDobSwitch,
     BabySwitch,
@@ -38,6 +35,8 @@ from ndoh_hub.celery import app
 from ndoh_hub.utils import get_mom_age, get_today, rapidpro
 from registrations.models import ClinicCode
 from registrations.tasks import request_to_jembi_api
+from requests.exceptions import RequestException
+from temba_client.exceptions import TembaHttpError
 
 logger = logging.getLogger(__name__)
 
@@ -532,3 +531,100 @@ def process_ada_assessment_notification(
     triage.risk = triage.calculate_risk()
     triage.full_clean()
     triage.save()
+
+
+if settings.TURN_URL and settings.TURN_TOKEN:
+    turn_api = settings.TURN_URL + "/v1/contacts/{}/messages"
+    turn_header = {
+        "Authorization": "Bearer {}".format(settings.TURN_TOKEN),
+        "Accept": "application/vnd.v1+json",
+    }
+
+if settings.RAPIDPRO_URL and settings.RAPIDPRO_TOKEN:
+    rapidpro_url = urljoin(settings.RAPIDPRO_URL, "/contact/read/{}/")
+
+
+@app.task(
+    autoretry_for=(RequestException, SoftTimeLimitExceeded, TembaHttpError),
+    retry_backoff=True,
+    max_retries=15,
+    acks_late=True,
+    soft_time_limit=10,
+    time_limit=15,
+)
+def post_random_contacts_to_slack_channel():
+    # modified before and after datetime
+    before = get_today()
+    after = get_today() - timedelta(days=16)
+    contact_details = []
+    try:
+        for contact_batch in rapidpro.get_contacts(
+            before=before, after=after
+        ).iterfetches(retry_on_rate_exceed=True):
+            for contact in contact_batch:
+                contact_uuid = contact.uuid
+                contact_urns = contact.urns
+
+                if contact_uuid and contact_urns:
+                    whatsapp_number = [
+                        i.split(":")[1] for i in contact_urns if "whatsapp" in i
+                    ]
+
+                    if whatsapp_number:
+                        contact = whatsapp_number[0]
+                        profile_link = get_turn_profile_link(contact)
+
+                        if profile_link:
+                            rapidpro_link = rapidpro_url.format(contact_uuid)
+                            links = {
+                                "Rapid Pro Link: ": rapidpro_link,
+                                "Turn Link": profile_link,
+                            }
+
+                            contact_details.append(links)
+
+        if contact_details:
+            sent = send_slack_message(contact_details)
+            return {"success": sent, "results": contact_details}
+        return {"success": False, "results": contact_details}
+    except TembaHttpError:
+        return {"success": False, "results": contact_details}
+
+
+def get_turn_profile_link(contact):
+    turn_link = None
+
+    if contact:
+        try:
+            turn_url = turn_api.format(contact)
+
+            profile = requests.get(url=turn_url, headers=turn_header)
+
+            if profile:
+                # Get turn message link
+                turn_link = profile.json().get("chat").get("permalink")
+        except RequestException:
+            return turn_link
+    return turn_link
+
+
+def send_slack_message(contact_details):
+    # Send message to slack
+    if settings.SLACK_URL and settings.SLACK_TOKEN:
+        try:
+            response = requests.post(
+                urljoin(settings.SLACK_URL, "/api/chat.postMessage"),
+                {
+                    "token": settings.SLACK_TOKEN,
+                    "channel": "test-mon",
+                    "text": contact_details,
+                },
+            ).json()
+
+            if response:
+                if response["ok"]:
+                    return True
+                return False
+        except RequestException:
+            return False
+    return False
--- eventstore/test_tasks.py
+++ eventstore/test_tasks.py
@@ -4,11 +4,10 @@ from unittest import mock
 
 import responses
 from django.test import TestCase, override_settings
-from temba_client.v2 import TembaClient
-
 from eventstore import tasks
 from eventstore.models import Covid19Triage, ImportError, ImportRow, MomConnectImport
 from registrations.models import ClinicCode
+from temba_client.v2 import TembaClient
 
 
 def override_get_today():
@@ -703,3 +702,132 @@ class ProcessAdaAssessmentNotificationTests(TestCase):
         )
         self.assertEqual(triage.created_by, "test"),
         self.assertEqual(triage.data, {"age": 30, "pregnant": False}),
+
+
+class PostRandomContactsToSlackTests(TestCase):
+    def setUp(self):
+        tasks.rapidpro = TembaClient("textit.in", "test-token")
+
+    @responses.activate
+    @override_settings(
+        RAPIDPRO_URL="https://rapidpro",
+        RAPIDPRO_TOKEN="rapidpro_token",
+        EXTERNAL_REGISTRATIONS_V2=True,
+    )
+    def test_get_contacts(self):
+        responses.add(
+            responses.GET,
+            "https://rapidpro/api/v2/contacts.json",
+            json=[
+                {
+                    "uuid": "148947f5-a3b6-4b6b-9e9b-25058b1b7800",
+                    "urns": ["whatsapp:27786159018"],
+                },
+                {
+                    "uuid": "128947f5-a3b6-4b3b-9e9b-25058b1b7801",
+                    "urns": ["whatsapp:27720001010", "tel:0102584697"],
+                },
+            ],
+        )
+
+        response = tasks.post_random_contacts_to_slack_channel()
+
+        self.assertEqual(type(response), dict)
+        self.assertEqual(type(response.get("results")), list)
+        self.assertIn("success", response)
+
+
+class GetTurnContactProfileTests(TestCase):
+    def setUp(self):
+        tasks.rapidpro = TembaClient("textit.in", "test-token")
+
+    @responses.activate
+    @override_settings(TURN_URL="http://turn", TURN_TOKEN="token")
+    def test_get_turn_profile_link(self):
+        responses.add(
+            responses.GET,
+            "http://turn/v1/contacts/27781234567/messages",
+            json={
+                "chat": {
+                    "assigned_to": None,
+                    "owner": "+27836378500",
+                    "permalink": "https://app.turn.io/c/68cc14-6a4e-4962-82ed-c576fdd",
+                    "state": "OPEN",
+                    "state_reason": "Re-opened by inbound message.",
+                    "unread_count": 0,
+                    "uuid": "68cc14b3-6a4e-4962-82ed-c572c6836fdd",
+                }
+            },
+            status=200,
+        )
+        response = tasks.get_turn_profile_link("27781234567")
+
+        self.assertEqual(type(response), str)
+        self.assertEqual(
+            str(response), "https://app.turn.io/c/68cc14b3-6a4e-4962-82ed-c572c6836fdd"
+        )
+        self.assertNotEqual(
+            type(response), "https://app.turn.io/c/68cc14b3-6a4e-4962-82ed-c572c6836fdz"
+        )
+
+    @responses.activate
+    def test_get_turn_profile_link_none_contact(self):
+        contact = None
+        responses.add(
+            responses.GET,
+            "http://turn/v1/contacts//messages",
+            json={
+                "chat": {
+                    "assigned_to": None,
+                    "owner": "+27836378500",
+                    "permalink": "https://app.turn.io/c/68cc13-6a4e-4962-82ed-c572cfdc",
+                    "state": "OPEN",
+                    "state_reason": "Re-opened by inbound message.",
+                    "unread_count": 0,
+                    "uuid": "68cc14b3-6a4e-4962-82ed-c572c6836fdc",
+                }
+            },
+            status=200,
+        )
+        response = tasks.get_turn_profile_link(contact)
+
+        self.assertEqual(response, None)
+
+
+class SendSlackMessageTests(TestCase):
+    def setUp(self):
+        self.contact_details = [
+            {
+                "Rapid_Pro_Link: ": "http://connect.co.za/contact/read/dc-7c-42-a1-a3/",
+                "Turn_Profile_Link": "http://app.turn.io/c/684b3-6ae-496-82d-c6c",
+            },
+            {
+                "Rapid_Pro_Link: ": "http://connect.co.za/contact/read/b2-59-4e-ac-fd/",
+                "Turn_Profile_Link": "http://app.turn.io/c/0e11a-13a2-4284-b961-3474d",
+            },
+            {
+                "Pro_Link: ": "http://connect.co.za/contact/read/341-2d1-49b-b0b-230c/",
+                "Profile_Link": "http://app.turn.io/c/cda524fb-80b9-40c0-a3e2-a452b797",
+            },
+        ]
+
+    @responses.activate
+    @override_settings(SLACK_URL="http://slack.com", SLACK_TOKEN="slack_token")
+    def test_send_slack_message(self):
+        responses.add(
+            responses.POST,
+            "http://slack.com/api/chat.postMessage",
+            json={
+                "ok": True,
+                "token": "slack_token",
+                "channel": "test-mon",
+                "text": self.contact_details,
+                "deleted": False,
+                "updated": 1_639_475_940,
+                "team_id": "T0CJ9CT7W",
+            },
+        )
+
+        response = tasks.send_slack_message(self.contact_details)
+
+        self.assertEqual(response, True)
