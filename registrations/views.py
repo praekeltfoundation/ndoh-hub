72f17538203c5964aec7d894e26e2af2eb9cbeb2
Erik Harding <erik@praekelt.org>
2022-01-25 13:54:43 +0200

    isort

--- registrations/views.py
+++ registrations/views.py
@@ -4,7 +4,6 @@ from rest_framework.permissions import DjangoModelPermissions
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-
 from .models import ClinicCode
 
 
