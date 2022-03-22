5b7c0d206860bb8c4917df0fa9e578be2e825318
Erik Harding <erik@praekelt.org>
2022-03-22 09:51:47 +0200

    black

--- mqr/migrations/0004_auto_20220322_0733.py
+++ mqr/migrations/0004_auto_20220322_0733.py
@@ -6,18 +6,28 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('mqr', '0003_auto_20220322_0539'),
+        ("mqr", "0003_auto_20220322_0539"),
     ]
 
     operations = [
         migrations.RenameField(
-            model_name='baselinesurveyresult',
-            old_name='danger_sign',
-            new_name='danger_sign2',
+            model_name="baselinesurveyresult",
+            old_name="danger_sign",
+            new_name="danger_sign2",
         ),
         migrations.AddField(
-            model_name='baselinesurveyresult',
-            name='danger_sign1',
-            field=models.CharField(choices=[('weight_gain', 'Weight gain of 4-5 kilograms'), ('vaginal_bleeding', 'Vaginal bleeding'), ('nose_bleeds', 'Nose bleeds'), ('skip', 'Skip')], default=None, max_length=20, null=True),
+            model_name="baselinesurveyresult",
+            name="danger_sign1",
+            field=models.CharField(
+                choices=[
+                    ("weight_gain", "Weight gain of 4-5 kilograms"),
+                    ("vaginal_bleeding", "Vaginal bleeding"),
+                    ("nose_bleeds", "Nose bleeds"),
+                    ("skip", "Skip"),
+                ],
+                default=None,
+                max_length=20,
+                null=True,
+            ),
         ),
     ]
