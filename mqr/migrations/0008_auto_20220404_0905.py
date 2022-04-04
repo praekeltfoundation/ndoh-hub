875ececb9e2e5fba453ee643afb80abab635c3f9
Buhle <buhle@praekelt.org>
2022-04-04 12:40:50 +0200

    reformat migration script

--- mqr/migrations/0008_auto_20220404_0905.py
+++ mqr/migrations/0008_auto_20220404_0905.py
@@ -6,13 +6,25 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('mqr', '0007_auto_20220323_0852'),
+        ("mqr", "0007_auto_20220323_0852"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='baselinesurveyresult',
-            name='marital_status',
-            field=models.CharField(choices=[('never_married', 'Never married'), ('married', 'Married'), ('separated_or_divorced', 'Separated or divorced'), ('widowed', 'Widowed'), ('partner_or_boyfriend', 'Have a partner or boyfriend'), ('skip', 'Skip')], default=None, max_length=20, null=True),
+            model_name="baselinesurveyresult",
+            name="marital_status",
+            field=models.CharField(
+                choices=[
+                    ("never_married", "Never married"),
+                    ("married", "Married"),
+                    ("separated_or_divorced", "Separated or divorced"),
+                    ("widowed", "Widowed"),
+                    ("partner_or_boyfriend", "Have a partner or boyfriend"),
+                    ("skip", "Skip"),
+                ],
+                default=None,
+                max_length=20,
+                null=True,
+            ),
         ),
     ]
