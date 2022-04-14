fa5c4787db64f04f513447aece1705fe7d0a46eb
devchima <developer.chima@gmail.com>
2022-04-14 00:51:23 -0400

    Fix black

--- ada/migrations/0014_adaassessment.py
+++ ada/migrations/0014_adaassessment.py
@@ -5,23 +5,29 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('ada', '0013_remove_redirecturl_url'),
-    ]
+    dependencies = [("ada", "0013_remove_redirecturl_url")]
 
     operations = [
         migrations.CreateModel(
-            name='AdaAssessment',
+            name="AdaAssessment",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('contact_id', models.UUIDField()),
-                ('assessment_id', models.UUIDField()),
-                ('step', models.IntegerField(null=True)),
-                ('optionId', models.IntegerField(blank=True, null=True)),
-                ('user_input', models.TextField(blank=True, null=True)),
-                ('title', models.TextField(blank=True, null=True)),
-                ('description', models.TextField(blank=True, null=True)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("contact_id", models.UUIDField()),
+                ("assessment_id", models.UUIDField()),
+                ("step", models.IntegerField(null=True)),
+                ("optionId", models.IntegerField(blank=True, null=True)),
+                ("user_input", models.TextField(blank=True, null=True)),
+                ("title", models.TextField(blank=True, null=True)),
+                ("description", models.TextField(blank=True, null=True)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
             ],
-        ),
+        )
     ]
