a36f51877c70402f412e1f37d9d7c9c78ddcaae0
devchima <developer.chima@gmail.com>
2023-03-28 06:33:03 -0400

    release v0.9.64 to develop

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ndoh-hub",
-    version="0.9.61",
+    version="0.9.64",
     url="http://github.com/praekeltfoundation/ndoh-hub",
     license="BSD",
     author="Praekelt Foundation",
