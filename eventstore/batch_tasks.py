6a23587032d8c4c56cff0781e9139e7c97470330
Erik Harding <erik@praekelt.org>
2023-04-03 13:59:42 +0200

    Fix datetime issue

--- eventstore/batch_tasks.py
+++ eventstore/batch_tasks.py
@@ -1,5 +1,6 @@
 from celery_batches import Batches
 from django.conf import settings
+from django.utils.dateparse import parse_date
 
 from eventstore.models import Event
 from eventstore.whatsapp_actions import handle_event
@@ -20,4 +21,5 @@ def bulk_insert_events(requests):
     events = Event.objects.bulk_create(data)
     if settings.ENABLE_EVENTSTORE_WHATSAPP_ACTIONS:
         for event in events:
+            event.timestamp = parse_date(event.timestamp)
             handle_event(event)
--- eventstore/test_batch_tasks.py
+++ eventstore/test_batch_tasks.py
@@ -1,16 +1,23 @@
 from django.test import TestCase
 
 from eventstore.batch_tasks import bulk_insert_events
-from eventstore.models import Event
+from eventstore.models import Event, DeliveryFailure
+from django.test import override_settings
 
 
 class UpdateTurnContactTaskTest(TestCase):
+    @override_settings(ENABLE_EVENTSTORE_WHATSAPP_ACTIONS=True)
     def test_batch_insert_events(self):
+
+        DeliveryFailure.objects.get_or_create(
+            contact_id="recipient_id", defaults={"number_of_failures": 0}
+        )
+
         bulk_insert_events.delay(
             message_id="message_id",
             recipient_id="recipient_id",
             timestamp="2023-10-11 12:33",
-            status="sent",
+            status="failed",
             created_by="test user",
             data={},
             fallback_channel=False,
@@ -20,4 +27,4 @@ class UpdateTurnContactTaskTest(TestCase):
 
         self.assertEqual("message_id", event.message_id)
         self.assertEqual("recipient_id", event.recipient_id)
-        self.assertEqual("sent", event.status)
+        self.assertEqual("failed", event.status)
