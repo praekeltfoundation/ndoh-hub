bc03e736f2a5817d7a7361fe27b9691086b2f7c6
Hlamalani <hlamalani@praekelt.org>
2023-05-08 09:18:45 +0200

    ussd registration

--- ndoh_hub/tests/test_views.py
+++ ndoh_hub/tests/test_views.py
@@ -1,8 +1,8 @@
+import responses
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-import responses
 
 
 class SendWhatsappTemplateTests(APITestCase):
