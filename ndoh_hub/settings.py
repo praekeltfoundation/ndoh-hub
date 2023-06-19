ef68e20c1a88a7ba45fba834fcdc2b8250b0ec57
Erik Harding <erik@praekelt.org>
2023-06-19 13:06:28 +0200

    Add privacy policy PDF

--- ndoh_hub/settings.py
+++ ndoh_hub/settings.py
@@ -178,6 +178,8 @@ STATICFILES_FINDERS = (
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
