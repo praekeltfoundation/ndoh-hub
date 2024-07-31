a04aaed72586690ae45e9ad30ba72f2258c58e1b
Mudiwa Matanda <mudiwa@Mudiwas-Air.home>
2024-07-31 12:32:03 +0200

    updated version tag

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ndoh-hub",
-    version="0.10.19",
+    version="0.10.20",
     url="http://github.com/praekeltfoundation/ndoh-hub",
     license="BSD",
     author="Praekelt Foundation",
