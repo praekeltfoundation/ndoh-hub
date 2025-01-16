c330764bab6251bb3326d7b0b1a688319e9c8dde
erikh360 <erikh360@gmail.com>
2025-01-16 12:20:36 +0200

    Add bulk update turn contacts script

---
+++ scripts/migrate_to_turn/update_turn_contacts_bulk.py
@@ -0,0 +1,28 @@
+import os
+import sys
+from urllib.parse import urljoin
+
+import requests
+
+TURN_URL = "https://whatsapp-praekelt-cloud.turn.io"
+
+
+def bulk_update_turn_contacts(filename):
+    data = open(filename, "rb")
+    url = urljoin(TURN_URL, "v1/contacts")
+    headers = {
+        "Authorization": f"Bearer {os.environ['TURN_TOKEN']}",
+        "content-type": "text/csv",
+    }
+    response = requests.post(url, data=data, headers=headers)
+
+    print(response.status_code)
+    
+    f = open(f"result_{filename}", "wb")
+    f.write(response.content)
+    f.close()
+
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    bulk_update_turn_contacts(filename)
