49a6fe1b8a91ffcba1602148e6d5347d7e5f8ae4
erikh360 <erikh360@gmail.com>
2026-01-13 15:26:34 +0200

    Add new fields, processing functions and tests

--- scripts/migrate_to_turn/process_fields.py
+++ scripts/migrate_to_turn/process_fields.py
@@ -29,6 +29,26 @@ def process_datetime(value):
         return value
 
 
+def process_opted_in(value):
+    if not value:
+        return "true"
+
+    value = str(value).upper()
+    if value == "TRUE":
+        return "false"
+    return "true"
+
+
+def process_truthy(value):
+    if value is None:
+        return "false"
+    value = str(value).strip()
+    if value.upper() in ("", "FALSE", "NO", "0"):
+        return "false"
+
+    return "true"
+
+
 def get_user_tier(contact):
     # TODO: figure out user tier based on rapidpro fields
     return None
@@ -54,7 +74,7 @@ def get_user_babies(contact):
         birth_date = datetime.fromisoformat(value)
         babies.append(
             {
-                "all_vaccines_received": "all",
+                "all_vaccines_received": "",
                 "appointments_attended": 0,
                 "baby_birth_day": birth_date.day,
                 "baby_birth_month": birth_date.month,
@@ -64,9 +84,23 @@ def get_user_babies(contact):
                 "next_vacc_month": 0,
                 "next_vacc_year": 0,
                 "pregnancy_edd": 0,
-                "vaccination_status_at_reg": "all",
+                "vaccination_status_at_reg": "",
             }
         )
 
     if babies:
         return json.dumps(babies)
+
+
+def process_baby_loss_status(contact):
+    loss_messaging = contact.fields.get("loss_messaging")
+    optout_reason = contact.fields.get("optout_reason")
+    if loss_messaging == "TRUE" and optout_reason == "baby_loss":
+        return "true"
+
+
+def process_pregnancy_loss_status(contact):
+    loss_messaging = contact.fields.get("loss_messaging")
+    optout_reason = contact.fields.get("optout_reason")
+    if loss_messaging == "TRUE" and optout_reason in ("miscarriage", "stillbirth"):
+        return "true"
